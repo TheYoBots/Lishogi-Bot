6433c11c78fa3ee4ae5d0a04dc87b610e40399b3
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2021-06-01 21:17:51 +0300

    Test bot update (#21)

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 import pytest
+import pytest_timeout
 import requests
 import time
 import yaml
-from shutil import copyfile
 import importlib
 lishogi_bot = importlib.import_module("lishogi-bot")
 
@@ -39,28 +39,35 @@ def run_bot(CONFIG, logging_level):
             except:
                 pass
             time.sleep(2)
-        game_id = li.challenge_ai()['id']
-        time.sleep(2)
-        games = li.get_ongoing_games()
-        game_ids = list(map(lambda game: game['gameId'], games))
-        for game in game_ids:
-            if game != game_id:
-                try:
-                    li.abort(game)
-                except:
-                    pass
+        while li.get_ongoing_games():
+            time.sleep(60)
+
+        @pytest.mark.timeout(300)
+        def run_test():
+            game_id = li.challenge_ai()['id']
             time.sleep(2)
-        lishogi_bot.start(li, user_profile, engine_factory, CONFIG, logging_level, None, one_game=True)
-        response = requests.get('https://lishogi.org/game/export/{}'.format(game_id))
-        response = response.text
-        response = response.lower()
-        response = response.split('\n')
-        result = list(filter(lambda line: 'result' in line, response))
-        result = result[0][9:-2]
-        color = list(filter(lambda line: 'sente' in line, response))
-        color = 'w' if username.lower() in color[0] else 'b'
-        win = result == '1-0' and color == 'w' or result == '0-1' and color == 'b'
-        assert win
+            games = li.get_ongoing_games()
+            game_ids = list(map(lambda game: game['gameId'], games))
+            for game in game_ids:
+                if game != game_id:
+                    try:
+                        li.abort(game)
+                    except:
+                            pass
+                time.sleep(2)
+            lishogi_bot.start(li, user_profile, engine_factory, CONFIG, logging_level, None, one_game=True)
+            response = requests.get('https://lishogi.org/game/export/{}'.format(game_id))
+            response = response.text
+            response = response.lower()
+            response = response.split('\n')
+            result = list(filter(lambda line: 'result' in line, response))
+            result = result[0][9:-2]
+            color = list(filter(lambda line: 'sente' in line, response))
+            color = 'w' if username.lower() in color[0] else 'b'
+            win = result == '1-0' and color == 'w' or result == '0-1' and color == 'b'
+            assert win
+
+        run_test()
     else:
         lishogi_bot.logger.error("{} is not a bot account. Please upgrade it to a bot account!".format(user_profile["username"]))
     games = li.get_ongoing_games()
