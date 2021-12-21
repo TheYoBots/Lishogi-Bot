0e5a19a4183d724844dec15c6535115fbda994aa
TheYoBots <yohaan.nathanjw@gmail.com>
2021-12-21 17:30:00 +0000

    Get files from shoginet

--- tests.py
+++ tests.py
@@ -18,21 +18,17 @@ def test_nothing():
 
 
 def download_yo():
-    response = requests.get('https://github.com/mizar/YaneuraOu/releases/download/v6.5.0/YaneuraOu-v6.5.0-windows.zip', allow_redirects=True)
-    with open('yo.zip', 'wb') as file:
+    response = requests.get('https://github.com/TheYoBots/shoginet/blob/windows/YaneuraOu-by-gcc.exe?raw=true', allow_redirects=True)
+    with open('yo.exe', 'wb') as file:
         file.write(response.content)
-    with zipfile.ZipFile('yo.zip', 'r') as zip_ref:
-        zip_ref.extractall('.')
-    copyfile('./windows/NNUE/YaneuraOu_NNUE-normal-clang++-avx2.exe', 'yo.exe')
 
 
 def download_suisho():
-    response = requests.get('https://github.com/Tama4649/Kristallweizen/blob/master/Kristallweizen_kaiV0.4.zip?raw=true', allow_redirects=True)
+    response = requests.get('https://github.com/WandererXII/shoginet/archive/refs/heads/main.zip', allow_redirects=True)
     with open('shogi.zip', 'wb') as file:
         file.write(response.content)
     with zipfile.ZipFile('shogi.zip', 'r') as zip_ref:
         zip_ref.extractall('.')
-    copyfile('./eval/nn.bin', 'shogi.bin')
 
 
 def run_bot(CONFIG, logging_level):
@@ -112,6 +108,7 @@ def test_bot():
     CONFIG['engine']['dir'] = './'
     CONFIG['engine']['name'] = 'yo.exe'
     CONFIG['engine']['usi_options']['BookFile'] = 'no_book'
+    CONFIG['engine']['usi_options']['EvalDir'] = './shoginet-main/eval'
     run_bot(CONFIG, logging_level)
 
 
