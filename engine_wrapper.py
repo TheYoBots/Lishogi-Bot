0ee13ba786878ee938591363bd175323812b2d35
TheYoBots <yohaan.nathanjw@gmail.com>
2022-01-26 14:17:36 +0000

    UCI to USI Works even with Fairy Stockfish

--- engine_wrapper.py
+++ engine_wrapper.py
@@ -16,7 +16,7 @@ def create_engine(config, variant):
     engine_options = cfg.get("engine_options")
     usi_options = cfg.get("usi_options", {}) or {}
     if variant not in ['Standard', 'From Position']:
-        usi_options['UCI_Variant'] = variant.lower()
+        usi_options['USI_Variant'] = variant.lower()
     commands = [engine_path]
     if engine_options:
         for k, v in engine_options.items():
