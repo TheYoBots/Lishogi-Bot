9053991fa159408561a14c8ef64e8cd8338f3acc
Daniel Dugovic <ddugovic@users.noreply.github.com>
2023-04-01 09:01:51 -0500

    Test using fairy-stockfish

--- tests.py
+++ tests.py
@@ -18,15 +18,6 @@ def test_nothing():
     assert True
 
 
-def download_yo():
-    response = requests.get("https://github.com/mizar/YaneuraOu/releases/download/v7.0.0/Suisho5-YaneuraOu-v7.0.0-windows.zip", allow_redirects=True)
-    with open("yo.zip", "wb") as file:
-        file.write(response.content)
-    with zipfile.ZipFile("yo.zip", "r") as zip_ref:
-        zip_ref.extractall(".")
-    shutil.copyfile(f"YaneuraOu_NNUE-tournament-clang++-sse42.exe", f"yo.exe")
-
-
 def run_bot(CONFIG, logging_level):
     lishogi_bot.logger.info(lishogi_bot.intro())
     li = lishogi_bot.lishogi.Lishogi(CONFIG["token"], CONFIG["url"], lishogi_bot.__version__, logging_level)
@@ -92,15 +83,9 @@ def run_bot(CONFIG, logging_level):
 def test_bot():
     logging_level = lishogi_bot.logging.INFO  # lishogi_bot.logging_level.DEBUG
     lishogi_bot.logging_configurer(logging_level, None)
-    download_yo()
-    lishogi_bot.logger.info("Downloaded YaneuraOu for NNUE")
     with open("./config.yml.default") as file:
         CONFIG = yaml.safe_load(file)
     CONFIG["token"] = TOKEN
-    CONFIG["engine"]["dir"] = "./"
-    CONFIG["engine"]["name"] = "yo.exe"
-    CONFIG["engine"]["usi_options"]["BookFile"] = "no_book"
-    CONFIG["engine"]["usi_options"]["NetworkDelay"] = 500
     run_bot(CONFIG, logging_level)
 
 
