f0a6f14889193beccdf1d723ea79c226f576c5e7
ScriptDeveloper2004 <150360179+ScriptDeveloper2004@users.noreply.github.com>
2024-01-28 19:59:01 +0530

    Update conversation.py
    Please check the latest Lichess API documentations .

--- conversation.py
+++ conversation.py
@@ -9,23 +9,23 @@ class Conversation:
         self.engine = engine
         self.xhr = xhr
         self.version = version
-        self.challengers = challenge_queue
+        self.challenge_queue = challenge_queue
 
     command_prefix = "!"
 
-    def react(self, line, game):
+    def react(self, line):
         logger.info(f'*** {self.game.url()} [{line.room}] {line.username}: {line.text.encode("utf-8")}')
-        if line.text[0] == self.command_prefix:
-            self.command(line, game, line.text[1:].lower())
+        if line.text.startswith(self.command_prefix):
+            self.command(line, line.text[1:].lower())
 
-    def command(self, line, game, cmd):
+    def command(self, line, cmd):
         if cmd == "commands" or cmd == "help":
             self.send_reply(line, "Supported commands: !wait (only applicable at the start of the game), !name, !howto, !eval, !queue")
-        elif cmd == "wait" and game.is_abortable():
-            game.ping(60, 120, 120)
+        elif cmd == "wait" and self.game['state']['status'] == 'started':
+            self.game.ping(60, 120, 120)
             self.send_reply(line, "Waiting 60 seconds...")
         elif cmd == "name":
-            name = game.me.name
+            name = self.game.me['name']
             self.send_reply(line, f"{name} running {self.engine.name()} (Lishogi-Bot v{self.version})")
         elif cmd == "howto":
             self.send_reply(line, "How to run: https://github.com/TheYoBots/Lishogi-Bot")
@@ -35,14 +35,14 @@ class Conversation:
         elif cmd == "eval":
             self.send_reply(line, "I don't tell that to my opponent, sorry.")
         elif cmd == "queue":
-            if self.challengers:
-                challengers = ", ".join([f"@{challenger.challenger_name}" for challenger in reversed(self.challengers)])
+            if self.challenge_queue:
+                challengers = ", ".join([f"@{challenger['challenger']['name']}" for challenger in reversed(self.challenge_queue)])
                 self.send_reply(line, f"Challenge queue: {challengers}")
             else:
                 self.send_reply(line, "No challenges queued.")
 
     def send_reply(self, line, reply):
-        self.xhr.chat(self.game.id, line.room, reply)
+        self.xhr.chat(self.game['id'], line.room, reply)
 
     def send_message(self, room, message):
         if message:
